ee952a30b4cef6551e313ed784dc357706533f24
unknown <kenseisan23@gmail.com>
2023-04-12 21:37:39 +0300

    Gitignore added

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         int amount = 15000;
-        int bonus = 20;gig
+        int bonus = 20;
         int result = amount / bonus;
 
         System.out.println("Бонусные мили:" + result);
